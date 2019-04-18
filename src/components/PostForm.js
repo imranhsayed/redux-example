9539ad7efad7092abefd9540552979e1ce84eacf
Imran Sayed <imranhsayed@gmail.com>
2019-04-18 18:16:26 +0530

    Add form fieldss

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,7 +3,20 @@ import React from 'react';
 class PostForm extends React.Component {
 	render() {
 		return(
-			<div>Post form</div>
+			<div>
+				<form>
+					<h3>Post form</h3>
+					<label>
+						Title:
+						<input type="text" name="title"/>
+					</label>
+					<label>
+						Body:
+						<input type="textarea" name="body"/>
+					</label>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
 		);
 	}
 }
