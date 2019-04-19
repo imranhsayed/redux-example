abe8261fa62dd31571d4a02732d0b5df054360be
Imran Sayed <imranhsayed@gmail.com>
2019-04-19 17:53:17 +0530

    Create a basic redux store without react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import Posts from "./components/Posts";
 
-import { Provider } from 'react-redux';
 import store from "./store";
 
 class App extends React.Component {
 
 	render() {
-		console.warn( 'came' );
 		return(
-				<Provider store={store}>
-					<Posts/>
-				</Provider>
+			<div>App</div>
 		);
 	}
 }
--- src/store.js
+++ src/store.js
@@ -1,26 +1,59 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import combinedReducer from './reducers';
-
-const initialState = {};
-
-const middleware = [thunk];
+import { createStore } from "redux";
 
 /**
- *
- * Creates store
- *
- * first param is a reducer function
- * second param is a preloaded state
- * third param is the enhancer
+ * Create a reducer function.
+ * It takes a state and an action and returns a new state for the store.
+ * @param state
+ * @param action
  */
-const store = createStore(
-	combinedReducer,
-	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-);
+const reducer = function ( state, action ) {
+	if ( 'INCREMENT' === action.type ) {
+		return state + action.payload;
+	}
+	if ( 'DECREMENT' === action.type ) {
+		return state - action.payload;
+	}
+	return  state;
+};
+
+/**
+ * Create store using the reducer function added above
+ * and pass an initial state.
+ */
+const store = createStore( reducer, 0 );
+
+/**
+ * Listen to the store using subscribe
+ * When Anything changes to the store. the function inside of it will be called.
+ * store.getState() return the current state value.
+ */
+store.subscribe( () => {
+	console.warn( 'Store Changed: State value =', store.getState() );
+} );
+
+/**
+ * Lets dispatch an action.
+ * When the below action is dispatched store.subscribe will call the method inside of it.
+ */
+store.dispatch( {
+	type: 'INCREMENT',
+	payload: 1
+} );
+store.dispatch( {
+	type: 'INCREMENT',
+	payload: 2
+} );
+store.dispatch( {
+	type: 'INCREMENT',
+	payload: 3
+} );
+store.dispatch( {
+	type: 'INCREMENT',
+	payload: 100
+} );
+store.dispatch( {
+	type: 'DECREMENT',
+	payload: 1
+} );
 
 export default store;
