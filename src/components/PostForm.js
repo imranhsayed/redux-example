6bce7669a38228457782b2348ffc46337946fb90
Imran Sayed <imranhsayed@gmail.com>
2019-04-19 11:57:44 +0530

    Add form submit logic

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class PostForm extends React.Component {
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			title: '',
+			body: ''
+		}
+	}
+
+	handleFormSubmit = ( event ) => {
+		event.preventDefault();
+	};
+
+	handleOnChangeInput = ( event ) => {
+		this.setState( { [event.target.name]: event.target.value } )
+	};
+
 	render() {
 		return(
 			<div>
-				<form>
+				<form onSubmit={this.handleFormSubmit}>
 					<h3>Post form</h3>
 					<label>
 						Title:
-						<input type="text" name="title"/>
+						<input type="text" name="title" onChange={this.handleOnChangeInput} />
 					</label>
 					<label>
 						Body:
-						<input type="textarea" name="body"/>
+						<input type="textarea" name="body" onChange={this.handleOnChangeInput} />
 					</label>
 					<button type="submit">Submit</button>
 				</form>
